5873b5d0a390a63d1362e5dc0540fc078295cd11
Oduro <tomislav207@gmail.com>
2015-05-14 15:59:20 +0200

    Update Questions.js

--- www/models/Questions.js
+++ www/models/Questions.js
@@ -101,6 +101,69 @@ models.Questions = {
 					a4: "Answer 4",
 					correctAnswer: "a1"
 				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B1",
+					correctAnswer: "1"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B2",
+					correctAnswer: "2"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B3",
+					correctAnswer: "3"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B4",
+					correctAnswer: "4"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B5",
+					correctAnswer: "5"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B6",
+					correctAnswer: "6"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B7",
+					correctAnswer: "7"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B8",
+					correctAnswer: "8"
+				}
+			},
+			{
+				questionType: "B",
+				question: {
+					text: "Quest B9",
+					correctAnswer: "9"
+				}
 			}
 		]
 	},
