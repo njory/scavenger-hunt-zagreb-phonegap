bfc0bcf31b50a733ddcdc72b6761d631b1d19418
aseskar <tazdingo9@gmail.com>
2015-05-16 11:37:39 +0200

    Dodane stranice za pomoc

--- www/controllers/Help.js
+++ www/controllers/Help.js
@@ -8,7 +8,13 @@ controllers.Help = {
 		view = views.Help,
 		navigationBar = views.NavigationBar;
 
+                view.on('touchstart', '.tile-qr .button-right', me.onShowNbHelp);
+                view.on('touchstart', '.tile-navbar .button-left', me.onShowQrHelp);
+                view.on('touchstart', '.tile-navbar .button-right', me.onShowAbcdHelp);
+                view.on('touchstart', '.tile-abcd .button-left', me.onShowNbHelp);
 		navigationBar.on('touchstart', '.left-button.back-from-help', me.onBack);
+                $("#help-navbar").hide(200);
+                $("#help-abcd").hide(200);
 	},
 	onBack: function() {
 		controllers.Common.switchNavigationBar(
@@ -21,6 +27,30 @@ controllers.Help = {
 
 		views.MainMenu.show(200);
 		views.Help.hide(200);
+                $("#help-navbar").hide(200);
+                $("#help-abcd").hide(200);
+		$("#help-qr").show(200);
+	},
+	onShowNbHelp: function() {
+                views.Help.hide(200);
+		$("#help-qr").hide(200);
+                $("#help-abcd").hide(200);
+		$("#help-navbar").show(200);
+                views.Help.show(200);
+	},
+	onShowQrHelp: function() {
+                views.Help.hide(200);
+		$("#help-navbar").hide(200);
+                $("#help-abcd").hide(200);
+		$("#help-qr").show(200);
+                views.Help.show(200);
+	},
+	onShowAbcdHelp: function() {
+                views.Help.hide(200);
+		$("#help-qr").hide(200);
+		$("#help-navbar").hide(200);
+                $("#help-abcd").show(200);
+                views.Help.show(200);
 	}
 };
 
